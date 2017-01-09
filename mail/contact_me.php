d69a15ceca36581cbbdd18720455c7f134c3631d
Katie <katie.register@me.com>
2017-01-09 18:44:28 -0500

    added new mail system

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,27 +1,23 @@
 <?php
-// check if fields passed are empty
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
 
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$message = $_POST['message'];
+// using SendGrid's PHP Library
+// https://github.com/sendgrid/sendgrid-php
+// If you are using Composer (recommended)
+require 'vendor/autoload.php';
+// If you are not using Composer
+// require("path/to/sendgrid-php/sendgrid-php.php");
+$from = new SendGrid\Email("Example User", "nick@gmail.com");
+$subject = "Sending with SendGrid is Fun";
+$to = new SendGrid\Email("Example User", "abiam222@gmail.com");
+$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+$apiKey = getenv('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
+');
+$sg = new \SendGrid($apiKey);
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
 
-// create email body and send it
-$to = 'abiam222@gmail.com'; // put your email address here
-$email_subject = "Contact form submitted by:  $name";
-$email_body = "You have received a new message. \n\n".
-				  " Here are the details:\n \nName: $name \n ".
-				  "Email: $email_address\n Message: \n $message";
-$headers = "From: noreply@KatieRegister.com\n";
-// Since this email form will be generated from your server. The From email address will be best using something like this noreply@yourdomain.com
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-return true;
+
 ?>
