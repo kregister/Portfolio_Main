1a52bc6b858ff8de4009bb6a59c99c4ea4e8d98c
Katie <katie.register@me.com>
2017-01-11 17:15:47 -0500

    image updates

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -3,21 +3,45 @@
 // using SendGrid's PHP Library
 // https://github.com/sendgrid/sendgrid-php
 // If you are using Composer (recommended)
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
 
- require 'vendor/autoload.php';
 
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+
+require 'vendor/autoload.php';
+use Mailgun\Mailgun;
+
+$mgClient = new Mailgun('e2a6a49ab0cb0710e93391521b3321f3');
+$domain = "sandbox88871dbdd0b745779f875bed7bcbc12b.mailgun.org";
+
+$result = $mgClient->sendMessage($domain, array(
+    'from'    => $email_address,
+    'to'      => "Katie.Register@me.com",
+    'subject' => 'Email From Contact Page',
+    'name'    => $name,
+    'text'    => $message
+));
 // If you are not using Composer
 // require("path/to/sendgrid-php/sendgrid-php.php");
-$from = new SendGrid\Email("Example User", "abiam222@gmail.com");
-$subject = "Sending with SendGrid is Fun";
-$to = new SendGrid\Email("Example User", "abiam222@gmail.com");
-$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
-$apiKey = getenv('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');
-$sg = new \SendGrid($apiKey);
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();
+// $from = new SendGrid\Email("Example User", "abiam222@gmail.com");
+// $subject = "Sending with SendGrid is Fun";
+// $to = new SendGrid\Email("Example User", "abiam222@gmail.com");
+// $content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
+// $mail = new SendGrid\Mail($from, $subject, $to, $content);
+// $apiKey = getenv('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');
+// $sg = new \SendGrid($apiKey);
+// $response = $sg->client->mail()->send()->post($mail);
+// echo $response->statusCode();
+// echo $response->headers();
+// echo $response->body();
 
 ?>
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit3af49016d624d3d5be47f48cf3403c92
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit3af49016d624d3d5be47f48cf3403c92', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
